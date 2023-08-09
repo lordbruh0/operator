689f5786bf15a2e18be0adccbd870306658d9750
Ryo/32/XR5 <140568034+lordbruh0@users.noreply.github.com>
2023-08-09 08:44:36 +0700

    Add files via upload

---
+++ src/JavaApplication7.java
@@ -0,0 +1,22 @@
+
+public class JavaApplication7{
+public static void main(String[]args){
+int bilangan1, bilangan2;
+
+bilangan1 = 13;
+bilangan2 = 5;
+
+int hasilpenjumlahan = bilangan1+bilangan2;
+int hasilpengurangan = bilangan1+bilangan2;
+int hasilpembagian = bilangan1/bilangan2;
+int hasilperkalian = bilangan1*bilangan2;
+int hasilmodulu = bilangan1%bilangan2;
+
+    System.out.println("hasil penjumlahan" + hasilpenjumlahan);
+    System.out.println("hasil pengurangan"+ hasilpengurangan);
+    System.out.println("hasil pembagian"+ hasilpembagian);
+    System.out.println("hasil perkalian" + hasilperkalian);
+    System.out.println("hasil modulus" + hasilmodulu);
+    
+}
+}
