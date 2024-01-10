b9db2ba30e61a6a26ab4d71d085801dbe4314798
Ryo/32/XR5 <140568034+lordbruh0@users.noreply.github.com>
2024-01-10 19:59:32 +0700

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,49 @@
+public class Bus {
+    private int penumpang, maxpenumpang;
+    private double beratTotal;
+
+    // konstruktor
+    public Bus(int maxpenumpang) {
+        this.maxpenumpang = maxpenumpang;
+        penumpang = 0;
+        beratTotal = 0;
+    }
+
+    // method mutator
+    public void pluspenumpang(int penumpang) {
+        int temp;
+        temp = this.penumpang + penumpang;
+        if (temp >= maxpenumpang) {
+            System.out.println("Overload penumpang");
+        } else {
+            this.penumpang = temp;
+        }
+    }
+
+    public void getPassword(int password) {
+        if (password == 90) {
+            System.out.println("password benar");
+        } else {
+            System.out.println("pass salah");
+        }
+    }
+
+    public void plusBerat(double berat) {
+        beratTotal += berat;
+    }
+
+    public double getAverage() {
+       
+            return beratTotal / penumpang;        
+        }
+
+
+    public void cetak() {
+        System.out.println("Penumpang sekarang = " + penumpang);
+        System.out.println("penumpang seharusnya adalah =" + maxpenumpang);
+        System.out.println("Berat sekarang = " + beratTotal);
+    }
+    public void cetak2(){
+        System.out.println("Berat rata rata = " + getAverage());
+    }
+}
---
+++ UjiCoba.java
@@ -0,0 +1,34 @@
+public class UjiCoba {
+    public static void main(String[] args) {
+   
+        Bus busMini=new Bus(10);
+        busMini.getPassword(40);
+        busMini.getPassword(90);
+        busMini.cetak();
+
+        busMini.pluspenumpang(1);
+        busMini.plusBerat(66.4);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(1);
+        busMini.plusBerat(87.9);
+        busMini.cetak();
+
+
+        busMini.pluspenumpang(1);
+        busMini.plusBerat(10.6);
+        busMini.cetak();
+
+        busMini.pluspenumpang(1);
+        busMini.plusBerat(49.6);
+        busMini.cetak();
+
+
+        busMini.pluspenumpang(1);
+        busMini.plusBerat(100.6);
+        busMini.cetak();
+        
+        busMini.cetak2();
+    }
+}
+
